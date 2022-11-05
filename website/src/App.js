cbaf2e9c268e4fcdac2288218545d61046e7e1c4
Lydia Yang <lydiayang92@gmail.com>
2022-11-05 16:34:52 -0700

    set up background image

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import logo from "./assets/beachHacksLogo.png";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <body></body>;
 }
 
 export default App;
