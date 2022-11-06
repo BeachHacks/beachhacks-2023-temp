2da27b3ace2f149d562fbd9db3ad35aa288dec50
Lydia Yang <lydiayang92@gmail.com>
2022-11-06 11:35:55 -0800

    added Home file. currently working on About page

--- website/src/App.js
+++ website/src/App.js
@@ -1,44 +1,9 @@
 import "./App.css";
-import logo from "./assets/beachHacksLogo.png";
 import dropDown from "./assets/scrollDownButton.png";
 import Grid from "@mui/material/Grid";
 
 function App() {
-  return (
-    <body>
-      <div className="frame-1">
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item xs={6}>
-            <img src={logo} alt="beachhacks logo" />
-          </Grid>
-          <Grid item xs={4}>
-            <h1>Coming Soon</h1>
-          </Grid>
-          <Grid item xs={2}>
-            <h2>February 25-26th, 2023</h2>
-          </Grid>
-          <Grid item xs={6}>
-            <h3>
-              Our website is under construction. We'll be here soon with our new
-              awesome site.
-            </h3>
-          </Grid>
-          <Grid item xs={6}>
-            <img
-              className="drop-down-spacing"
-              src={dropDown}
-              alt="dropdown button"
-            />
-          </Grid>
-        </Grid>
-      </div>
-    </body>
-  );
+  return <body></body>;
 }
 
 export default App;
---
+++ website/src/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+import logo from "./assets/beachHacksLogo.png";
+import dropDown from "./assets/scrollDownButton.png";
+import Grid from "@mui/material/Grid";
+
+function Home() {
+  return (
+    <div className="frame-1">
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={6}>
+          <img src={logo} alt="beachhacks logo" />
+        </Grid>
+        <Grid item xs={4}>
+          <h1>Coming Soon</h1>
+        </Grid>
+        <Grid item xs={2}>
+          <h2>February 25-26th, 2023</h2>
+        </Grid>
+        <Grid item xs={6}>
+          <h3>
+            Our website is under construction. We'll be here soon with our new
+            awesome site.
+          </h3>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            className="drop-down-spacing"
+            src={dropDown}
+            alt="dropdown button"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Home;
