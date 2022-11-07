86f0aaa5498a801b34f94a6addd8c0654a181a78
Jordan Hilado <jordanalihilado@gmail.com>
2022-11-06 19:11:30 -0800

    added icon library, setup sponsor page

--- website/src/Sponsor.js
+++ website/src/Sponsor.js
@@ -1,16 +1,10 @@
 import "./styles/App.css";
-
-import dropDown from "./assets/scrollDownButton.png";
-import student from "./assets/icons/student.png";
-import earth from "./assets/icons/earth.png";
-import clock from "./assets/icons/clock.png";
-import folder from "./assets/icons/folder.png";
-
+import { Button } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { Button } from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { Link } from "@mui/material";
 
 import React from "react";
 
@@ -36,9 +30,51 @@ function Sponsor() {
             direction="row"
             justifyContent="space-evenly"
             alignItems="center"
+            mb={8}
           >
             <Grid item>
-              <Button variant="contained">Sponorship Packet</Button>
+              <Button
+                href="https://tinyurl.com/BeachHacks2023-SponsorPacket"
+                variant="contained"
+                target="_blank"
+              >
+                Sponorship Packet
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item display="flex" mr={4}>
+              <Typography mt={0.5}>
+                <MailIcon />
+              </Typography>
+              <Link
+                href="mailto:beachhacks@csulb.acm.org"
+                color="inherit"
+                target="_blank"
+              >
+                <Typography variant="h5" ml={2}>
+                  beachhacks@csulb.acm.org
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid ml={4} item display="flex">
+              <Typography mt={0.5}>
+                <InstagramIcon />
+              </Typography>
+              <Link
+                href="https://www.instagram.com/beachhackscsulb/?hl=en"
+                color="inherit"
+                target="_blank"
+              >
+                <Typography variant="h5" ml={2}>
+                  beachhackscsulb
+                </Typography>
+              </Link>
             </Grid>
           </Grid>
         </div>
